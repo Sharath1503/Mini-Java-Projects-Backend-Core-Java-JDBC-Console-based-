6bb85169f578a61c28e311b4038ac462c42510f9
Sharath1503 <chitikelasharath@gmail.com>
2025-08-29 16:49:41 +0530

    Create DefaultConstructorEx.java

---
+++ Constructors/DefaultConstructorEx.java
@@ -0,0 +1,15 @@
+public class DefaultConstructorEx {
+	
+	public DefaultConstructorEx()
+	{
+		System.out.println("This is Default Constructor");
+		
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DefaultConstructorEx obj = new DefaultConstructorEx();
+		
+	}
+
+}
