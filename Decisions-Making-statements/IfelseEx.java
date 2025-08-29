6eebf04ca918cc46e5076bc2105b2a7320fea2dc
Sharath1503 <chitikelasharath@gmail.com>
2025-08-29 16:53:13 +0530

    Create IfelseEx.java

---
+++ Decisions-Making-statements/IfelseEx.java
@@ -0,0 +1,20 @@
+
+public class IfelseEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a =10;
+		int b=20;
+		
+		if (a>b)
+		{
+			System.out.println("A value is bigger than B");
+		}
+		else
+		{
+			System.out.println("A value is smaller than B");
+		}
+
+	}
+
+}
