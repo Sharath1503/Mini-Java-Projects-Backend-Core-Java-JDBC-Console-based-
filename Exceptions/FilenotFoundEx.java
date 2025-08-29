3f5d315cc86b1a8e78c6eac8fae0ad93bf4cd05e
Sharath1503 <chitikelasharath@gmail.com>
2025-08-29 16:56:59 +0530

    Add files via upload

---
+++ Exceptions/FilenotFoundEx.java
@@ -0,0 +1,23 @@
+package com.exceptions.example;
+
+public class FilenotFoundEx {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		try {
+			FileReader fr = new FileReader
+		}
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+	}
+
+}
